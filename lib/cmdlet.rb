5e8ab72d13ba572b95855def5b37093c2508c3b2
David Cruwys <david@ideasmen.com.au>
2022-07-08 10:53:58 +1000

    first commit

---
+++ lib/cmdlet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cmdlet/version"
+
+module Cmdlet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cmdlet/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Cmdlet
+  VERSION = "0.1.0"
+end
