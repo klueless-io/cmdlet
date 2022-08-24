72a14a843e08015425c8f2fba44b4a8ccd175a85
David Cruwys <david@ideasmen.com.au>
2022-08-24 10:22:38 +1000

    fix: add reset to cmdlet configuration

--- spec/cmdlet/cmdlet_configuration_spec.rb
+++ spec/cmdlet/cmdlet_configuration_spec.rb
@@ -14,4 +14,35 @@ RSpec.describe Cmdlet::CmdletConfiguration do
       padr_char: ' '
     )
   end
+
+  context 'when altered' do
+    before do
+      instance.padl_count = 20
+      instance.padl_char = '-'
+      instance.padr_count = 20
+      instance.padr_char = '-'
+    end
+
+    it do
+      is_expected.to have_attributes(
+        padl_count: 20,
+        padl_char: '-',
+        padr_count: 20,
+        padr_char: '-'
+      )
+    end
+
+    context 'when reset' do
+      before { instance.reset }
+
+      it do
+        is_expected.to have_attributes(
+          padl_count: 30,
+          padl_char: ' ',
+          padr_count: 30,
+          padr_char: ' '
+        )
+      end
+    end
+  end
 end
